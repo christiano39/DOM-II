a6615d8ac7d8c4dcfc4671c3e48265cd6bf1271a
christiano39 <carneson39@gmail.com>
2020-06-01 22:25:39 -0600

    added first eventlistener on the h1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+// H1 color change on hover
+mainHeading = document.querySelector('h1.logo-heading');
+
+function randomColorOnHover(e){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    e.target.style.color = `rgb(${r}, ${g}, ${b})`;
+}
+
+function backToDefaultColor(e){
+    e.target.style.color = '#212529';
+}
+
+mainHeading.addEventListener('mouseenter', randomColorOnHover);
+mainHeading.addEventListener('mouseleave', backToDefaultColor);
