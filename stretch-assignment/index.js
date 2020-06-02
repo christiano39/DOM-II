8ee0be13664ca594d4124264060e05f8553c12e4
christiano39 <carneson39@gmail.com>
2020-06-02 14:54:08 -0600

    rockets travel to the right on mousedown and return to original position on mouseup

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -5,10 +5,24 @@ const pinkBlock = document.querySelector('.block--pink');
 const grayBlock = document.querySelector('.block--gray');
 const blocks = Array.from(document.querySelectorAll('.block'));
 let order = 0;
+let interval;
 
 blocks.forEach(block => {
+    let move = 0;
     block.addEventListener('click', e => {
         order--;
         block.style.order = `${order}`; 
     });
+    block.addEventListener('mousedown', e => {
+        interval = setInterval(function(){
+            move += 1;
+            block.style.transform = `translateX(${move}px)`;
+        }, 10);
+    });
+    window.addEventListener('mouseup', e => {
+        clearInterval(interval);
+        move = 0;
+        block.style.transform = `translateX(${move}px)`;
+    });
 });
+
